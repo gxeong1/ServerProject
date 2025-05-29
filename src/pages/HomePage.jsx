e51137a63060d361cbf56c4b20a6a2a4b9f0fd1e
gxeong1 <156269967+gxeong1@users.noreply.github.com>
2025-05-29 20:39:24 +0900

    홈화면 프론트엔드

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../styles/HomePage.css'
+
+const HomePage = () => {
+    return(
+        <div className="home-container">
+            <img 
+                className="main-icon"
+                src="/icon/Icon1.svg"
+                alt="Logo"/>
+
+            <div className="btn-container">
+                <button className="login-btn">Google로 로그인</button>
+                <button className="guest-btn">게스트 계정 이용하기</button>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
